07bd7592ab22e197c01a44e16e02d2833f6f0a9e
Srishti Ahlawat <164470881+Srishti-Ahlawat@users.noreply.github.com>
2024-09-25 11:54:27 +0530

    Update main.tf

--- modules/action-groups/main.tf
+++ modules/action-groups/main.tf
@@ -1,7 +1,6 @@
 data "azurerm_key_vault" "kv" {
   name                = var.key_vault_name
   resource_group_name = var.key_vault_rg_name
-  xyz = xyz
 }
 
 data "azurerm_key_vault_secret" "pagerduty_integration_key_secret" {
