6b93d3de174ac85e46f756e3963060c3a63a43f3
Srishti Ahlawat <164470881+Srishti-Ahlawat@users.noreply.github.com>
2024-09-26 12:56:26 +0530

    Add files via upload

---
+++ modules/private-link-service/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  frontend_ip_configurations = flatten([
+    for lb in data.azurerm_lb.this : [
+      for config in lb.frontend_ip_configuration : {
+        name = config.name
+        id   = config.id
+      }
+    ]
+  ])
+  frontend_ip_configurations_map = {
+    for config in local.frontend_ip_configurations : config.name => config.id
+  }
+}
