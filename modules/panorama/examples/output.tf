4c31d2c840c2db0c8ad6ee903e186aa27fbeb935
Srishti Ahlawat <164470881+Srishti-Ahlawat@users.noreply.github.com>
2024-11-07 16:47:09 +0530

    Delete modules/panorama directory

--- modules/panorama/examples/output.tf
+++
@@ -1,9 +0,0 @@
-output "mgmt_ip_address" {
-  description = "Panorama management IP address. If `public_ip` was `true`, it is a public IP address, otherwise a private IP address."
-  value       = module.example-panorama.mgmt_ip_address
-}
-
-output "interface" {
-  description = "Panorama network interface. The `azurerm_network_interface` object."
-  value       = module.example-panorama.interface
-}
--- modules/panorama/output.tf
+++
@@ -1,9 +0,0 @@
-output "mgmt_ip_address" {
-  description = "Panorama management IP address. If `public_ip` was `true`, it is a public IP address, otherwise a private IP address."
-  value       = module.vmseries-modules_panorama.mgmt_ip_address
-}
-
-output "interface" {
-  description = "Panorama network interface. The `azurerm_network_interface` object."
-  value       = module.vmseries-modules_panorama.interface
-}
